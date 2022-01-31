eb4384cb58443f6a2e0c5482c54afe6bc461a2fd
Katuhito Hara <rahakatuhito@gmail.com>
2022-01-31 11:59:09 +0900

    timeline templates

---
+++ timeline/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Katuapp001Config(AppConfig):
+    name = 'katuapp001'
---
+++ timeline/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import generic
+
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
+
+index =IndexView.as_view()
+
+

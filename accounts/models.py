75502cbc0d6676716cced0cc28f1e6bfc47d9038
Katuhito Hara <rahakatuhito@gmail.com>
2022-02-01 17:01:04 +0900

    model migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from matplotlib.image import thumbnail
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    desctiption = models.TextField(verbose_name='プロフィール', null=True, blank=True)
+    photo = models.ImageField(verbose_name='写真', blank=True, null=True, upload_to='images/')
+    thumbnail = ImageSpecField(source='photo', processors=[ResizeToFill(256, 256)], format='JPEG', options={'quality': 60})
+
+    class Meta:
+        verbose_name_plural = 'CustomUser'
+        
+
